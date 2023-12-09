fc0d040def8b024121a66068b2e695eeb3a38d13
Vasster <vasilhspinhs@gmail.com>
2023-12-09 16:29:54 +0200

    Improved Enemy Ai with distance manager

--- DungeonSlasherUnity/DungeonSlasherUnity/Dungeon_Slasher/Assets/Scripts/EnemyAI.cs
+++ DungeonSlasherUnity/DungeonSlasherUnity/Dungeon_Slasher/Assets/Scripts/EnemyAI.cs
@@ -4,73 +4,58 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
-    private IEnumerator coroutine; //refrence to our coroutine
-    float seconds = 2f;
-    public PlayerController playerController;
-    public HitPlayer EnemyAttacksPlayer;
-    public EnemyAI EnemyScript;
-    public AudioSource GoblinDeath;
     public float Speed;
     public Transform target;
     public float minimumDistance;
     public float timeBetweenAttacks;
     private float nextAttackTime;
     public Animator enemyAnim;
-    Vector3 Enemy;
-    Vector3 AttackTarget;
-    public LayerMask playerLayer;
-    public int offset = 3;
     public float EnemyHealth = 100;
-    public float Losehealth = 100;
     public bool isAttackingPlayer;
+
     private void Start()
     {
-        transform.LookAt(target); // enemyPrefab look at player
-        AttackTarget = FindObjectOfType<PlayerController>().transform.position; // we find the knightmovement position
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        target = player.transform;
+        transform.LookAt(target);
     }
+
     void Update()
     {
-       
-        if (Vector3.Distance(transform.position, target.position) >= minimumDistance)//if the distance is >= the minimum distance
+        if (Vector3.Distance(transform.position, target.position) >= minimumDistance)
         {
             transform.LookAt(target);
-            transform.position = Vector3.MoveTowards(transform.position, target.position, Speed * Time.deltaTime);//we transform the position of enemyPrefab towars our player position
-            enemyAnim.SetBool("IsAttacking", false);//we set the enemyPrefab bool isattacking to false distance is >= the minimumDistance
-         if (Vector3.Distance(transform.position, target.position) <= minimumDistance ) // if it is <= the minimum distance
-        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, Speed * Time.deltaTime);
+            enemyAnim.SetBool("isAttacking", false);
+            if (Vector3.Distance(transform.position, target.position) <= minimumDistance)
+            {
                 transform.LookAt(target);
-                enemyAnim.SetBool("IsAttacking", true);// we set our bool isattacking to true
-                AttackOurPlayer();
-              
+                if (Time.time >= nextAttackTime)
+                {
+                    isAttackingPlayer = true;
+                    enemyAnim.SetBool("isAttacking", true);
+                    Debug.Log("AttackingPlayer");
+                    nextAttackTime = Time.time + timeBetweenAttacks;
+                }
             }
-           
         }
+      
+    }
 
-        }
-    
-     public void LoseHealth(int damage)//enemyPrefab loses health
+    public void LoseHealth(int damage)
     {
-        EnemyHealth -=damage;// our enemyPrefab health -= the damage
-        if (EnemyHealth <= 0)//if enemyPrefab health is below zero or equal
+        EnemyHealth -= damage;
+        if (EnemyHealth <= 0)
         {
-            enemyAnim.SetBool("IsDead", true);//we set the bool to dead is true
-            GoblinDeath.Play();
-            EnemyScript.enabled = false;//we disable the enemyPrefab script
-            Destroy(this.gameObject,2f);//we destroy the enemyPrefab
+            enemyAnim.SetTrigger("Dead");
+            Destroy(this.gameObject, 2f);
+            Debug.Log("EnemyDied" + this.gameObject.name);
         }
     }
 
-    void AttackOurPlayer()
+    private void OnDrawGizmos()
     {
-
-        {
-            isAttackingPlayer = true;
-            EnemyAttacksPlayer.AttackOurPlayer();//we attack our player
-         //   playerController.TakeDamage();
-            Debug.Log("Enemy attacked Player");
-        }
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.position, minimumDistance);
     }
-       
-    }
-
-
+}
